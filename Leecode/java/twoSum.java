882a3d20ff908e1a0bbeb538d6555d59c1f43167
chenyaowu <30755338+chenyaowu@users.noreply.github.com>
2019-02-28 10:12:02 +0800

    Update twoSum.java

--- Leecode/java/twoSum.java
+++ Leecode/java/twoSum.java
@@ -1,36 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-  		int sum ,t = 0;
-		ListNode result = new ListNode(0);
-		ListNode temp = result;
-		while(l1 != null || l2 != null){
-			
-			int val1 = 0,val2 = 0;
-			if(l1 != null){ val1 = l1.val;}
-			if(l2 != null){val2 = l2.val;}
-			
-			sum = val1 + val2 + t;
-			t = sum/10;
-			
-			temp.next = new ListNode(sum%10);
-			temp = temp.next;
-			
-			if(l1 != null){	l1 = l1.next;}
-			if(l2 != null){l2 = l2.next;}
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length-1);
+    	for (int i = 0; i < nums.length; i++) {
+    		if(map.get(nums[i]) == null ){
+    			map.put(target-nums[i], i);
+    		}else{
+    			result[0] = map.get(nums[i]);
+    			result[1] = i; 
+    		}
 		}
-		
-		if(t != 0){	temp.next = new ListNode(t);}
-		
-		return result.next;
-
-
+    	return result;
     }
-}
+} 
