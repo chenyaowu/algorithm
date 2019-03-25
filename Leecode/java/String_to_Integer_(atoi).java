3611b7ca6b61c373b77c186d7cfd69b1df4d173e
chenyaowu <30755338+chenyaowu@users.noreply.github.com>
2019-03-25 16:09:48 +0800

    format
    format

--- Leecode/java/String_to_Integer_(atoi).java
+++ Leecode/java/String_to_Integer_(atoi).java
@@ -1,56 +1,36 @@
 class Solution {
     public int myAtoi(String str) {
  	str = str.trim();
-		if (str.equals("")) {
-			return 0;
-		}
+		if (str.equals("")) { return 0; }
 		String numStr = "";
 		int i = 0;
 		char t = str.charAt(i);
 		boolean b = false;
-        int num = 0;
+        	int num = 0;
 		while (t == '+' || t == '0') {
-			if(t == '+'){
-				num ++;
-			}
-
+			if(t == '+'){ num++; }
 			i++;
-			if (str.length() == i) {
-				return 0;
-			}
+			if (str.length() == i) { return 0; }
 			t = str.charAt(i);
-
 		}
-		if(num>1){
-			return 0;
-		}
+		if(num>1){ return 0; }
 		if (t == '-') {
-			if (i != 0|| str.length() == 1) {
-				return 0;
-			} else {
-                b = true;
-				t = str.charAt(++i);
-			}
+			if (i != 0 || str.length() == 1) { return 0; } 
+			else { b = true; t = str.charAt(++i); }
 		}
 
 		while (t > 47 && t < 58) {
-            i++;
-			if (t == '0' && numStr.length() != 0) {
-				numStr += t;
-			} else if (t != '0') {
-				numStr += t;
-			}
+           		i++;
+			if (t == '0' && numStr.length() != 0) { numStr += t; } 
+			else if (t != '0') { numStr += t; }
 
-			if (i == str.length()) {
-				break;
-			}
+			if (i == str.length()) { break; }
 			t = str.charAt(i);
 		}
 		
-		if (numStr.equals("")) {
-			return 0;
-		}
-        numStr = b ? "-" + numStr : numStr;
+		if (numStr.equals("")) { return 0; }
+	    
+        	numStr = b ? "-" + numStr : numStr;
 		if (b && numStr.length() > 11) {
 			return Integer.MIN_VALUE;
 		} else if (!b && numStr.length() > 10) {
@@ -61,7 +41,6 @@ class Solution {
 			return Integer.MAX_VALUE;
 		}
 
-
 		return Integer.parseInt(numStr);
 
     }
@@ -70,11 +49,8 @@ class Solution {
 		for (int i = 0; i < str.length(); i++) {
 			char a = str.charAt(i);
 			char b = otherStr.charAt(i);
-			if (a > b) {
-				return true;
-			}else if(a<b){
-				return false;
-			}
+			if (a > b) { return true;}
+			else if(a<b){ return false;}
 		}
 		return false;
 	}
