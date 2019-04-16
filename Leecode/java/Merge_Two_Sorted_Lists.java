e4503c21cd2afa78c00bbf6505472c8b86c62e62
chenyaowu <30755338+chenyaowu@users.noreply.github.com>
2019-04-16 09:57:22 +0800

    add Merge_Two_Sorted_Lists
    add Merge_Two_Sorted_Lists

---
+++ Leecode/java/Merge_Two_Sorted_Lists.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+		ListNode temp = head;
+		while (l1 != null && l2 != null) {
+			if (l1.val > l2.val) {
+				temp.next = l2;
+				l2 = l2.next;
+			} else {
+				temp.next = l1;
+				l1 = l1.next;
+			}
+			temp = temp.next;
+		}
+
+		if (l1 == null) {
+			while (l2 != null) {
+				temp.next = l2;
+				l2 = l2.next;
+				temp = temp.next;
+			}
+		}
+		if (l2 == null) {
+			while (l1 != null) {
+				temp.next = l1;
+				l1 = l1.next;
+				temp = temp.next;
+			}
+		}
+
+		return head.next;
+    }
+}
