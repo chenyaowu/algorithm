cccdea6a4f69c3a6c1d8a47d1360fc3b7567ea7a
chenyaowu <chenyaowu15@gmail.com>
2019-06-15 16:53:21 +0800

    add Array
    add Array

---
+++ algorithm/Array/src/Array.java
@@ -0,0 +1,145 @@
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    // 获取数组大小
+    public int getSize() {
+        return size;
+    }
+
+    // 获取容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    // 数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public void add(int index, E e) {
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Index out of array");
+        }
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size= %d, capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index out of array");
+        }
+
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index out of array");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 删除指定位置的元素，并返回
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index out of array");
+        }
+
+        E res = data[index];
+        for (int i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        data[size] = null;
+
+        if (size == data.length / 4 && data.length != 0) {
+            resize(data.length / 2);
+        }
+        return res;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+}
