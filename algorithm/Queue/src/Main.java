eb18836c6b320d643b12d9efa3a33fcc6bef795d
chenyaowu <chenyaowu15@gmail.com>
2019-07-06 23:20:58 +0800

    add linkedlist
    add linkedlist

--- algorithm/Queue/src/Main.java
+++ algorithm/Queue/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Main {
 
-    private static double testQueue(Queue<Integer> q,int opCount){
+    private static double testQueue(Queue<Integer> q, int opCount) {
         long startTime = System.currentTimeMillis();
 
         Random random = new Random();
@@ -21,13 +21,15 @@ public class Main {
     public static void main(String[] args) {
         int opCount = 100000;
 
-        ArrayQueue<Integer>  arrayQueue = new ArrayQueue<>();
-        double time1 = testQueue(arrayQueue,opCount);
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
         LoopQuquq<Integer> loopQuquq = new LoopQuquq<Integer>();
-        double time2 = testQueue(loopQuquq,opCount);
+        double time2 = testQueue(loopQuquq, opCount);
         System.out.println("ArrayQueue, time:" + time1 + "s");
         System.out.println("loopQuquq, time:" + time2 + "s");
 
-
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        double time3 = testQueue(linkedListQueue, opCount);
+        System.out.println("linkedListQueue, time:" + time3 + "s");
     }
 }
--- algorithm/Stack/src/Main.java
+++ algorithm/Stack/src/Main.java
@@ -1,15 +1,32 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+    private static double testStack(Stack<Integer> stack, int opCount){
+        long startTime = System.currentTimeMillis();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
 
-        stack.pop();
-        System.out.println(stack);
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
 
+        long endTime = System.currentTimeMillis();
 
+        return (endTime - startTime) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+      int opCount = 10000000;
+
+        ArrayStack<Integer> stack = new ArrayStack<>();
+
+        LinkedListStack<Integer> stack2 = new LinkedListStack<>();
+
+        System.out.println(testStack(stack,opCount));
+        System.out.println(testStack(stack2,opCount));
     }
 }
