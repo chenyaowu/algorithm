3a9869c9f471f4bebe40137ef91aadb503c2cf20
chenyaowu <chenyaowu15@gmail.com>
2020-03-31 22:16:38 +0800

    update
    update

--- algorithm/HashTable/src/HashTable.java
+++ algorithm/HashTable/src/HashTable.java
@@ -2,24 +2,24 @@ import java.util.TreeMap;
 
 public class HashTable<K, V> {
 
-    private static final int upperTol = 10;
-    private static final int lowerTol = 2;
-    private static final int initCapacity = 10;
+    private final int[] capacity = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+            49157, 98417, 196613, 393241, 786433, 1527869, 3145739, 6291469,
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610621741};
+    private static final int upperTol = 10; //上界
+    private static final int lowerTol = 2; //下界
+    private int capacityIndex = 0;
     private TreeMap<K, V>[] hashTable;
     private int M; //长度
     private int size;
 
-    public HashTable(int M) {
-        this.M = M;
+
+    public HashTable() {
+        this.M = capacity[capacityIndex];
         size = 0;
         hashTable = new TreeMap[M];
         for (int i = 0; i < M; i++) {
             hashTable[i] = new TreeMap<>();
         }
-    }
-
-    public HashTable() {
-        this(initCapacity);
     }
 
     public int hash(K key) {
@@ -38,8 +38,9 @@ public class HashTable<K, V> {
             map.put(key, value);
             size++;
 
-            if (size >= upperTol * M) {
-                resize(2 * M);
+            if (size >= upperTol * M && capacityIndex + 1 < capacity.length) {
+                capacityIndex++;
+                resize(capacity[capacityIndex]);
             }
         }
     }
@@ -51,8 +52,9 @@ public class HashTable<K, V> {
             ret = map.remove(key);
             size--;
 
-            if (size < lowerTol * M && M / 2 >= initCapacity) {
-                resize(M / 2);
+            if (size < lowerTol * M && capacityIndex - 1 >= 0) {
+                capacityIndex--;
+                resize(capacity[capacityIndex]);
             }
         }
         return ret;
